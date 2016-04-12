7019aafd616ee4bc41af27f86120fccf694aaf87
Anoop Sarkar <anoop@sfu.ca>
2016-04-12 15:57:21 -0700

    testing out new json bibtex approach

--- public/bibtex/convert-bibtex.py
+++ public/bibtex/convert-bibtex.py
@@ -1,6 +1,15 @@
 import bibtexparser, json
 from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import convert_to_unicode
+from bibtexparser.bwriter import BibTexWriter
+from bibtexparser.bibdatabase import BibDatabase
+
+def add_raw_bibtex(contents):
+    parser = BibTexParser()
+    bib = bibtexparser.loads(contents, parser=parser)
+    writer = BibTexWriter()
+    for i in bib.entries:
+        print writer.write(i)
 
 def convert_bibtex(contents):
     parser = BibTexParser()
