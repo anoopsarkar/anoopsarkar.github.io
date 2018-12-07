c709ea357febf15adb4b25fddef357deb03f7512
Anoop Sarkar <anoop+github@cs.sfu.ca>
2018-12-07 14:54:37 -0800

    use bundle in sfu_push.sh

--- sfu_push.sh
+++ sfu_push.sh
@@ -1,2 +1,2 @@
-jekyll build --config=_config.yml,_sfuconfig.yml
+bundle exec jekyll build --config=_config.yml,_sfuconfig.yml
 rsync --delete -avz _site/ anoop@linux.cs.sfu.ca:/fas-info/cs/people/Faculty/anoop
